7f9e9ee872a85b1a0ca2c2abca02ea018f14e48f
Andrew Hall <andrewchall92@gmail.com>
2023-12-04 20:48:57 -0800

    Styled the nav links and added a resume download. I also added a picture and styled the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import "./style.css";
 
 function About() {
     return (
-      <section>
-        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      <section class="home-page">
+        <div class= "card">
+          <img src="../../../Assets/me.jpg" class="card-img-left" alt="picture of author" />
+          <p> 
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
           Cumque, molestiae non cupiditate quas vel aspernatur quasi assumenda nisi voluptate necessitatibus quod expedita 
-          consequuntur, omnis iste repellendus dolor corporis voluptatibus sequi!</p>
+          consequuntur, omnis iste repellendus dolor corporis voluptatibus sequi!
+          </p>
+        </div>
       </section>
     );
   }
 
   export default About;
+
+  
--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
+import "./style.css";
 
 function NavTabs() {
 
   return (
-    <ul>
-      <li>
+    <ul class= "nav justify-content-end">
+      <li class="nav-item" >
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li class="nav-item">
         <Link to="/ProjectsPage">Projects</Link>
       </li>
-      <li>
+      <li class="nav-item">
         <Link to="/ContactPage">Contact</Link>
       </li>
+      <li class="nav-item">
+      <a href="../../Assets/documents/Resume.pdf" download>Resume</a>
+    </li>
     </ul>
   );
 }
