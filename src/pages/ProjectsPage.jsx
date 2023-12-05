338454071e24934c161b02240d8d9989ed0b626a
Andrew Hall <andrewchall92@gmail.com>
2023-12-04 21:32:52 -0800

    Added a couple starter projects to get the basic layout of the portfolio page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,9 +3,53 @@ import React from "react";
 function ProjectsPage() {
   return (
     <>
-      <h1>Projects Page</h1>
+      <h1>Portfolio</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img
+              src="../../Assets/current-day.png"
+              class="img-fluid"
+              alt="picture of work scheduler"
+            />
+            <a
+              href="https://github.com/Andrewchall92/calendar-app"
+              target="_blank"
+            >
+              View the repository
+            </a>
+            <a
+              href="https://andrewchall92.github.io/calendar-app/"
+              target="_blank"
+            >
+              View the application online
+            </a>
+          </div>
+          <div class="col-md-6">
+            <img
+              src="../../Assets/dashboard.png"
+              class="img-fluid"
+              alt="picture of readme generator"
+            />
+            <a
+              href="https://github.com/Andrewchall92/weather-app"
+              target="_blank"
+            >
+              View the repository
+            </a>
+            <a
+              href="https://andrewchall92.github.io/weather-app/"
+              target="_blank"
+            >
+              View the application online
+            </a>
+          </div>
+        </div>
+         {/* more projects will be added */}
+        <h4>More coming soon!</h4>
+      </div>
     </>
   );
-};
+}
 
 export default ProjectsPage;
